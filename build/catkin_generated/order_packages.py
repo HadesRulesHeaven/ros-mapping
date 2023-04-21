ea7a5190de68e4a5141cf97bec8ac70a0a5f2249
HadesRulesHeaven <f20220218@pilani.bits-pilani.ac.in>
2023-04-21 12:54:18 +0530

    Commit catkin_ws

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ritvik/catkin_ws/src'
+whitelisted_packages = 'mobile_manipulator_body'.split(';') if 'mobile_manipulator_body' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ritvik/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ritvik/catkin_ws/devel;/opt/ros/noetic' != '' else []
